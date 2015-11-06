99b03b243aa141c0b988f38b9f8c8ef81c8e3411
lukasz.wypych <ishimura92@gmail.com>
2015-11-06 20:48:59 +0100

    Fixed Android Annotationss issues and first build

--- app/src/main/java/com/example/luki/inzynierka/MainActivity.java
+++ app/src/main/java/com/example/luki/inzynierka/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import org.androidannotations.annotations.EActivity;
 
-@EActivity(R.id.main_activity_layout)
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
 
