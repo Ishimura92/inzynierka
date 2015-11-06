f36971c3b23de4264158532e820b17da9c8ab843
lukasz.wypych <ishimura92@gmail.com>
2015-11-06 20:38:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/luki/inzynierka/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.luki.inzynierka;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.id.main_activity_layout)
+public class MainActivity extends AppCompatActivity {
+
+
+}
