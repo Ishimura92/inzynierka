148876494be5cf1a3cc076f7709c475bc7d040a8
lukasz.wypych <ishimura92@gmail.com>
2015-11-07 22:00:44 +0100

    Little rework of Vehicle chooser view

--- app/src/main/java/com/example/luki/inzynierka/Adapters/VehicleListAdapter.java
+++ app/src/main/java/com/example/luki/inzynierka/Adapters/VehicleListAdapter.java
@@ -41,11 +41,33 @@ public class VehicleListAdapter extends RecyclerView.Adapter<VehicleListAdapter.
     public void onBindViewHolder(CustomViewHolder customViewHolder, int position) {
         final Vehicle vehicle = vehicles.get(position);
 
-        customViewHolder.textViewVehicleTitle.setText(vehicle.getBrand() + " " + vehicle.getModel());
-        customViewHolder.textViewVehicleDescription.setText(vehicle.getEngineType());
-        customViewHolder.imageViewVehicleIcon.setImageResource(R.drawable.car_placeholder);
-        //TODO dodać ustawianie ikonki samochodu na podstawe nadwozia
+        customViewHolder.textViewVehicleBrand.setText(vehicle.getBrand());
+        customViewHolder.textViewVehicleModel.setText(vehicle.getModel());
+        customViewHolder.textViewEngineType.setText(vehicle.getEngineType());
+        customViewHolder.textViewEngineCapacity.setText(String.valueOf(vehicle.getEngineCapacity()));
+        customViewHolder.textViewBodyType.setText(vehicle.getBodyType());
 
+
+        switch (vehicle.getBodyType()){
+            case "Sedan":
+                customViewHolder.imageViewVehicleIcon.setImageResource(R.drawable.ic_sedan_dark);
+                break;
+            case "Kombi":
+                customViewHolder.imageViewVehicleIcon.setImageResource(R.drawable.ic_kombi_dark);
+                break;
+            case "Minivan/Van":
+                customViewHolder.imageViewVehicleIcon.setImageResource(R.drawable.ic_van_dark);
+                break;
+            case "Coupe/Kabrio":
+                customViewHolder.imageViewVehicleIcon.setImageResource(R.drawable.ic_coupe_dark);
+                break;
+            case "SUV":
+                customViewHolder.imageViewVehicleIcon.setImageResource(R.drawable.ic_suv_dark);
+                break;
+            case "Hatchback":
+                customViewHolder.imageViewVehicleIcon.setImageResource(R.drawable.ic_hatchback_dark);
+                break;
+        }
 
         customViewHolder.cardViewVehicleItem.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,16 +87,22 @@ public class VehicleListAdapter extends RecyclerView.Adapter<VehicleListAdapter.
 
     public class CustomViewHolder extends RecyclerView.ViewHolder {
         protected ImageView imageViewVehicleIcon;
-        protected TextView textViewVehicleTitle;
-        protected TextView textViewVehicleDescription;
+        protected TextView textViewBodyType;
+        protected TextView textViewEngineCapacity;
+        protected TextView textViewEngineType;
+        protected TextView textViewVehicleBrand;
+        protected TextView textViewVehicleModel;
         protected CardView cardViewVehicleItem;
 
         public CustomViewHolder(View view) {
             super(view);
             this.cardViewVehicleItem = (CardView) view.findViewById(R.id.vehicleItemLayout);
             this.imageViewVehicleIcon = (ImageView) view.findViewById(R.id.imageViewVehicleIcon);
-            this.textViewVehicleTitle = (TextView) view.findViewById(R.id.textViewVehicleTitle);
-            this.textViewVehicleDescription = (TextView) view.findViewById(R.id.textViewVehicleDescription);
+            this.textViewVehicleBrand = (TextView) view.findViewById(R.id.textViewVehicleBrand);
+            this.textViewVehicleModel = (TextView) view.findViewById(R.id.textViewVehicleModel);
+            this.textViewBodyType = (TextView) view.findViewById(R.id.textViewBodyType);
+            this.textViewEngineCapacity = (TextView) view.findViewById(R.id.textViewEngineCapacity);
+            this.textViewEngineType = (TextView) view.findViewById(R.id.textViewEngineType);
         }
     }
 }
